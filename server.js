180555f0fcd3008bca097454fb82f282b6af82f7
BroadwayanLamb <s012467@students.lmsd.org>
2019-09-12 20:46:21 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require("http");
 var fio = require("fs");
 var qs = require("querystring");
 
+const data = ["firstName", "lastName", "grade", "snum", "email"];
+
 http.createServer(function (req, res) {
 	res.writeHead(200, { 'Content-Type': 'text/html' } );
 
@@ -26,7 +28,31 @@ http.createServer(function (req, res) {
 				res.write(data);
 				res.end();
 			});
-			fio.appendFileSync("members.list", JSON.stringify(form) + "\n");
+			
+			let str = "";
+			let first = true;
+			
+			if (!fio.existsSync("members.list")){
+				for (index in data){
+					str += (!first? "," : "") + data[index];
+					first = false;
+				}
+				fio.appendFileSync("members.list", str + "\n");
+			}
+			
+			str = "";
+			first = true;
+			
+			for (index in data){
+				try{
+					str += (!first? ",\"" : "\"") + form[data[index]].replace("\"", "\"\"") + "\"";
+					first = false;
+				} catch(e) {
+					return;
+				}
+			}
+			
+			fio.appendFileSync("members.list", str + "\n");
 		});
 		
 	}
